1dbbbd37d3214f8f8ad125246b250cd2ac7dfc7f
haji <muhamedhajajii@outlook.com>
2024-06-11 11:21:15 +0300

    'dist'

---
+++ src/app/core/services/core-values.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { Corevalues } from '../interfaces/corevalues';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoreValuesService {
+  coreValues: Corevalues[] = [
+    {
+      title: 'الجودة',
+      pragraph:
+        'نلتزم بتقديم تعليم عالي الجودة يلبي احتياجات وتطلعات جميع طلابنا',
+      image_1: 'assets/Home/Core-values/1.png',
+      image_2: 'assets/About/about-section-2/1.png',
+    },
+    {
+      title: 'التعاون',
+      pragraph:
+        'نقدر أهمية التعاون ودور المجتمع، ونسعى لتعزيز الروابط بين المتعلمين والمربين وخبراء الصناعة، ',
+      image_1: 'assets/Home/Core-values/2.png',
+      image_2: 'assets/About/about-section-2/2.png',
+    },
+    {
+      title: 'التفوق الأكاديمي',
+      pragraph:
+        'نسعى لتعزيز التفوق الأكاديمي من خلال برامج تعليمية مبتكرة ومتطورة.',
+      image_1: 'assets/Home/Core-values/3.png',
+      image_2: 'assets/About/about-section-2/3.png',
+    },
+    {
+      title: 'التعاون',
+      pragraph:
+        'نعزز روح التعاون بين الطلاب والمعلمين وأولياء الأمور لتحقيق أهدافنا التعليمية المشتركة.',
+      image_1: 'assets/Home/Core-values/4.png',
+      image_2: 'assets/About/about-section-2/4.png',
+    },
+    {
+      title: 'التنمية المستدامة',
+      pragraph:
+        'نهدف إلى تحقيق التنمية المستدامة من خلال تعليم مستدام يعزز من قدرات الطلاب على المدى الطويل.',
+      image_1: 'assets/Home/Core-values/5.png',
+      image_2: 'assets/About/about-section-2/5.png',
+    },
+    {
+      title: 'الابتكار',
+      pragraph:
+        'نشجع الابتكار في أساليب التدريس ونستخدم أحدث التقنيات لتحسين تجربة التعلم عن بُعد',
+      image_1: 'assets/Home/Core-values/6.png',
+      image_2: 'assets/About/about-section-2/6.png',
+    },
+    {
+      title: 'التعلم مدى الحياة',
+      pragraph:
+        'نحن ندعم السعي وراء المعرفة في كل مرحلة من مراحل الحياة. كما نلتزم بتعزيز ثقافة تقدر التعلم المستمر',
+      image_1: 'assets/Home/Core-values/7.png',
+      image_2: 'assets/About/about-section-2/7.png',
+    },
+    {
+      title: 'التنوع والشمولية',
+      pragraph:
+        'نؤمن بأهمية التنوع ونحرص على توفير بيئة تعليمية شاملة ترحب بجميع الطلاب من مختلف الخلفيات.',
+      image_1: 'assets/Home/Core-values/8.png',
+      image_2: 'assets/About/about-section-2/8.png',
+    },
+    {
+      title: 'المسؤولية',
+      pragraph:
+        'نتحمل المسؤولية عن توفير بيئة تعليمية آمنة ومحفزة لجميع طلابنا.',
+      image_1: 'assets/Home/Core-values/9.png',
+      image_2: 'assets/About/about-section-2/9.png',
+    },
+    {
+      title: 'الشفافية',
+      pragraph:
+        'نلتزم بالشفافية في جميع جوانب عملنا ونتواصل بوضوح وصراحة مع جميع الأطراف المعنية',
+      image_1: './assets/Home/Core-values/10.png',
+      image_2: 'assets/About/about-section-2/10.png',
+    },
+  ];
+}
---
+++ src/app/core/services/team.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import { Team } from '../interfaces/team';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TeamService {
+  Team: Team[] = [
+    {
+      ImageUrl: 'assets/Home/Home-Team/1.jpg',
+      Instractor_Name: 'جورج أمجد',
+      Instractor_Job: 'الرئيس التنفيذي لمدرسة summit',
+      Instractor_Info: [
+        'حاصل على بكاليريوس الهندسة المدنية من جامعة الإسكندرية',
+        'استشاري بمشروع دلتا مصر أضخم مشروعا بجمهورية مصر العربية لاستصلاح الأراضي الزراعية',
+        'حاصل على شهادة إدارة التعليم الإلكتروني (Certified E- Learning Manager- CELM)',
+        'شهادة تدريب المعلمين في التعليم الافتراضي (Virtual Teacher Training Certification)',
+        'شهادة في إدارة المشاريع التعليمية (Educational Project Management Certification)',
+        'مدرس mathematics ببعض المدارس الدولية سابقا',
+      ],
+    },
+    {
+      ImageUrl: 'assets/Home/Home-Team/2.jpg',
+      Instractor_Name: 'عبد الرحمن العربي',
+      Instractor_Job: 'استشاري التطوير بمدرسه ساميت',
+      Instractor_Info: [
+        'خريج جامعة علوم لسكندرية قسم بايوكيمستري بتقدير امتياز مع مرتبه الشرف',
+        'ساهم ف تطوير اكتر مدرسه أون لاين بدايته من المراحل الأولى وصولا لمراحل متقدمة',
+        'مقدم دورات في تدريب المعلمين علي طرق التدريس اعتمادا علي النمط السويدي',
+        'خبره في مجال تطوير المدارس اللون لاين 5 سنين',
+        'درست لأكتر من ٢٠٠٠ طالب أون لاين واكترى من ١٢٠٠ أوف لاين',
+      ],
+    },
+    {
+      ImageUrl: 'assets/Home/Home-Team/3.jpg',
+      Instractor_Name: 'مايكل ميشيل',
+      Instractor_Job: 'مدير الجودة والاعتماد الأكاديمي',
+      Instractor_Info: [
+        'حاصل على ماجستير الترجمة ف اللغة الأسبانية من كليه الألسن جامعة عين شمس',
+        'مساعد سفيرة جمهورية الدومينيكان',
+        'business specialist في مايكروسوفت',
+        'professional advisor في تومتوم',
+        '⁠مترجم في معرض السلاح الدولي معين من وزارة الدفاع ووزارة الشباب',
+      ],
+    },
+    {
+      ImageUrl: 'assets/Home/Home-Team/4.jpg',
+      Instractor_Name: 'ماريان أشرف',
+      Instractor_Job: 'مديره الإرشاد الأكاديمي ومسئوله قسم اللغة الألمانية',
+      Instractor_Info: [
+        'حاصله على ماجستير الترجمة ف اللغة الألمانية من كليه الألسن جامعة كفر الشيخ',
+        'عضو ف لجنة تنظيم اليوم الثقافي الألماني',
+        'إخصائيه لدعم لسلسله فنادق ETABE ف الأقصر',
+        'عضو ف لجنة تنظيم مهرجان الأقصر للسينيما الأفريقه',
+        'مديره حضانة harvest الخاصة',
+      ],
+    },
+    {
+      ImageUrl: 'assets/Home/Home-Team/5.jpg',
+      Instractor_Name: 'أحمد سراج',
+      Instractor_Job: 'مدير تكنولوجيا التعليم',
+      Instractor_Info: [
+        'بكالوريوس حاسبات وذكاء اصطناعي جامعة بنها',
+        'ITI intensive code camp grad',
+        'Software Quality control Engineer at Systems Egypt',
+        'Software Quality control Engineer at Banque misr',
+      ],
+    },
+    {
+      ImageUrl: 'assets/Home/Home-Team/6.jpg',
+      Instractor_Name: 'ساره محمد',
+      Instractor_Job: 'مسؤول تطوير المدرسين',
+      Instractor_Info: [
+        'STEAMify academy founder',
+        'international certified trainer',
+        'STEAM certified',
+        '10 years of experience national and international schools',
+        'biotechnology graduate',
+      ],
+    },
+    {
+      ImageUrl: 'assets/Home/Home-Team/7.jpg',
+      Instractor_Name: 'جوزيف رفيق',
+      Instractor_Job: 'مسئول قسم اللغة الإنجليزية',
+      Instractor_Info: [
+        'محاضر بريطاني معتمد من جامعة كامبريدج.',
+        'خبرة تفوق 5 سنوات في تدريس اللغة الإنجليزية لغير الناطقين بها.',
+        'عمل كمتحدث رسمي ومدرب لغوي في العديد من المؤتمرات الدولية.',
+        'مشارك في برامج تطوير المناهج الدراسية بالتعاون مع مؤسسات تعليمية دولية.',
+      ],
+    },
+  ];
+  getAllTeamInfo(): any {}
+}
